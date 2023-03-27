2c7e8da0b5f1459386f40cd022da49142c8ee823
Purvish Shah <purvish14@gmail.com>
2023-03-27 15:54:25 +0100

    Binomial Implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,44 @@
+import binomialHeap.dataStructure.binomialHeap.BinomialHeap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Binomial heap implementation");
+        BinomialHeap binHeap = new BinomialHeap();
+
+        // Inserting in the binomial heap
+        // Custom input integer values
+        binHeap.insert(12);
+        binHeap.insert(8);
+        binHeap.insert(5);
+        binHeap.insert(15);
+        binHeap.insert(7);
+        binHeap.insert(2);
+        binHeap.insert(9);
+
+        // Size of binomial heap
+        System.out.println("Size of the binomial heap is "
+                + binHeap.getSize());
+
+        // Displaying the binomial heap
+        binHeap.displayHeap();
+
+        // Deletion in binomial heap
+//        binHeap.delete(15);
+//        binHeap.delete(8);
+        System.out.println(binHeap.extractMin());
+        System.out.println(binHeap.extractMin());
+        System.out.println(binHeap.extractMin());
+
+        // Size of binomial heap
+        System.out.println("Size of the binomial heap is "
+                + binHeap.getSize());
+
+        // Displaying the binomial heap
+        binHeap.displayHeap();
+
+        // Making the heap empty
+        binHeap.makeEmpty();
+
+        // checking if heap is empty
+        System.out.println(binHeap.isEmpty());
     }
 }
