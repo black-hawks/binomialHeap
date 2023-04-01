c38a50cc335a484adfe4c481862f48141193cc54
Purvish Shah <purvish14@gmail.com>
2023-04-01 20:52:28 +0100

    Add log entries

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ClearingHouse clearingHouse = new ClearingHouse();
         while (!clearingHouse.isEOD()) {
+            System.out.println(clearingHouse.getCurrentTime());
             BuyBinomialMaxHeap buyOrders = clearingHouse.fetchBuyOrders();
             BinomialHashMap sellOrders = clearingHouse.fetchSellOrders();
             clearingHouse.performTransactions(buyOrders, sellOrders);
--- src/main/java/stockExchange/market/ClearingHouse.java
+++ src/main/java/stockExchange/market/ClearingHouse.java
@@ -5,6 +5,7 @@ import stockExchange.dataStructure.binomialHeap.BuyBinomialMaxHeap;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Date;
 
 public class ClearingHouse {
     private final OrderReader buyReader;
@@ -46,18 +47,19 @@ public class ClearingHouse {
     public void performTransactions(BuyBinomialMaxHeap buyOrders, BinomialHashMap sellOrders) {
         pendingBuyOrders = new BuyBinomialMaxHeap();
         pendingSellOrders = new BinomialHashMap();
+        long buyOrdersCompleted = 0;
+        long buyOrdersPartiallyCompleted = 0;
         while (!buyOrders.isEmpty()) {
             Order order = buyOrders.extractHighestPriorityElement().getKey();
             Long quantityFulfilled = sellOrders.fetchOrder(order.getPrice(), order.getQuantity());
             if (quantityFulfilled != 0) {
                 if (quantityFulfilled == order.getQuantity()) {
-                    buyOrders.extractHighestPriorityElement();
-                    System.out.println(order + " fulfilled");
+                    buyOrdersCompleted++;
                 } else {
                     long remainingQuantity = order.getQuantity() - quantityFulfilled;
                     order.setQuantity(remainingQuantity);
                     pendingBuyOrders.insert(order);
-                    System.out.println(order + " partially fulfilled, remaining quantity: " + remainingQuantity);
+                    buyOrdersPartiallyCompleted++;
                 }
             } else {
                 //insert the node in the new binomial tree
@@ -65,9 +67,22 @@ public class ClearingHouse {
             }
         }
         pendingSellOrders = sellOrders;
+        System.out.println("Buy orders completed: " + buyOrdersCompleted);
+        System.out.println("Buy orders partially completed: " + buyOrdersPartiallyCompleted);
+        System.out.println("Sell orders completed: " + sellOrders.ordersCompleted);
+        System.out.println("Sell orders partially processed: " + buyOrdersPartiallyCompleted);
+        System.out.println();
     }
 
     public boolean isEOD() throws IOException {
         return buyReader.getTime() == -1 || sellReader.getTime() == -1;
+    }
+
+    public Date getCurrentTime() throws IOException {
+        long currentTime = buyReader.getTime();
+        if (currentTime == -1) {
+            return null;
+        }
+        return new Date(currentTime * 1000);
     }
 }
